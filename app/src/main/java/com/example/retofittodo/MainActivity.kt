0fff9c6ce611ee8feed50e7f38c85208ad22292f
vedantdalvi45 <vedalvi16@gmail.com>
2025-09-30 19:56:10 +0530

    Add : TodoAdapter and RecyclerView

--- app/src/main/java/com/example/retofittodo/MainActivity.kt
+++ app/src/main/java/com/example/retofittodo/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.example.retofittodo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retofittodo.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupRecyclerView()
+//
+        lifecycleScope.launch {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitInstance.api.getTodos()
+            } catch (e: Exception) {
+                Log.e(TAG, e.toString())
+                binding.progressBar.isVisible = false
+                return@launch
+            }
+            if(response.isSuccessful && response.body() != null) {
+                todoAdapter.todos = response.body()!!
+            }else{
+                Log.e(TAG, "Response not successful")
+            }
+            binding.progressBar.isVisible = false
+        }
     }
+    private fun setupRecyclerView() = binding.todoList.apply {
+        todoAdapter = TodoAdapter()
+        adapter = todoAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+
 }
